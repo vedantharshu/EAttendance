e7059fa0f81ec5f106d912715b44905f300a1450
Ojasva8 <vermaojasva8@gmail.com>
2020-07-12 22:56:09 +0530

    colors.xml file updated

---
+++ app/src/main/java/com/example/eattendance/AttendanceListActivity.java
@@ -0,0 +1,112 @@
+package com.example.eattendance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AttendanceListActivity extends AppCompatActivity {
+
+    private static final String TAG="AttendanceListActivity";
+    ListView listView;
+    Button submit;
+    AttendanceListAdapter adapter;
+    private StringBuffer present;
+    private StringBuffer absent;
+    private int presentCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attendance_list);
+        Log.d(TAG,"AttendanceListActivity: Started");
+        listView=(ListView)findViewById(R.id.attendanceLV);
+        AttendanceItem ojasva=new AttendanceItem("1","Ojasva",false);
+        AttendanceItem vedant=new AttendanceItem("2","Vedant",false);
+        AttendanceItem shivansh=new AttendanceItem("3","Shivansh",false);
+        AttendanceItem jagrat=new AttendanceItem("4","Jagrat",false);
+        AttendanceItem A=new AttendanceItem("5","A",false);
+        AttendanceItem B=new AttendanceItem("6","B",false);
+        AttendanceItem C=new AttendanceItem("7","C",false);
+        AttendanceItem D=new AttendanceItem("8","D",false);
+        AttendanceItem E=new AttendanceItem("9","E",false);
+        AttendanceItem F=new AttendanceItem("10","F",false);
+        AttendanceItem G=new AttendanceItem("11","G",false);
+        AttendanceItem H=new AttendanceItem("12","H",false);
+        AttendanceItem I=new AttendanceItem("13","I",false);
+        AttendanceItem J=new AttendanceItem("14","J",false);
+        AttendanceItem K=new AttendanceItem("15","K",false);
+
+
+
+
+        ArrayList<AttendanceItem> studentList=new ArrayList<>();
+        studentList.add(ojasva);
+        studentList.add(vedant);
+        studentList.add(shivansh);
+        studentList.add(jagrat);
+        studentList.add(A);
+        studentList.add(B);
+        studentList.add(C);
+        studentList.add(D);
+        studentList.add(E);
+        studentList.add(F);
+        studentList.add(G);
+        studentList.add(H);
+        studentList.add(I);
+        studentList.add(J);
+        studentList.add(K);
+
+
+        adapter=new AttendanceListAdapter(this,R.layout.activity_attendance_list,studentList);
+        listView.setAdapter(adapter);
+
+        presentCount=0;
+
+        present = new StringBuffer();
+        present.append("Present:\n");
+
+        absent = new StringBuffer();
+        absent.append("Present:\n");
+
+        submit=(Button)findViewById(R.id.submit_attenBTN);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                List<AttendanceItem> atList =adapter.attendanceList;
+                for(int i=0;i<atList.size();i++){
+                    AttendanceItem attendance = atList.get(i);
+                    if(attendance.isSelected()){
+                        present.append("\n" + attendance.getName());
+                        presentCount++;
+                    }
+                    else
+                    {
+                        absent.append("\n"+attendance.getName());
+                    }
+
+                }
+                present.append("\nTotal Present: "+String.valueOf(presentCount));
+
+                Toast.makeText(getApplicationContext(),present, Toast.LENGTH_LONG).show();
+
+                finish();
+            }
+        });
+
+
+
+
+    }
+}
--- app/src/main/java/com/example/eattendance/TeacherActivity.java
+++ app/src/main/java/com/example/eattendance/TeacherActivity.java
@@ -1,14 +1,103 @@
 package com.example.eattendance;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class TeacherActivity extends AppCompatActivity {
+    private Spinner classSpinner,sectionSpinner,subjectSpinner;
+    ArrayList<String> classes;
+    ArrayList<String> sections;
+    ArrayList<String> subjects;
+
+    Button mark;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher);
+        classSpinner = (Spinner) findViewById(R.id.class_spinner);
+        sectionSpinner = (Spinner) findViewById(R.id.section_spinner);
+        subjectSpinner = (Spinner) findViewById(R.id.subject_spinner);
+
+        classes=new ArrayList<>();
+        sections=new ArrayList<>();
+        subjects=new ArrayList<>();
+
+        classes.add("1");
+        classes.add("2");
+        classes.add("3");
+
+        sections.add("A");
+        sections.add("B");
+        sections.add("C");
+
+        subjects.add("Physics");
+        subjects.add("Chemistry");
+        subjects.add("Maths");
+
+
+
+        ArrayAdapter<String> class_adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_spinner_item,
+                classes
+        );
+        class_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        classSpinner.setAdapter(class_adapter);
+
+        ArrayAdapter<String> section_adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_spinner_item,
+                sections
+        );
+        section_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sectionSpinner.setAdapter(section_adapter);
+        mark=(Button)findViewById(R.id.mark_attendanceBTN);
+        mark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TeacherActivity.this,AttendanceListActivity.class));
+            }
+        });
+
+        ArrayAdapter<String> subject_adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_spinner_item,
+                subjects
+        );
+        subject_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        subjectSpinner.setAdapter(subject_adapter);
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("This will exit you.");
+        builder.setMessage("Do you want to exit? ");
+        builder.setPositiveButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+        builder.setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                finish();
+            }
+        });
+        builder.show();
     }
 }
